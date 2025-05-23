1aa7404e302f9284f6f9e3e722a5e81aded14186
YuliiaZas <julia.kv101@gmail.com>
2025-05-22 20:13:59 -0400

    feat: add main part of query

--- src/routes/graphql/rootQuery.ts
+++ src/routes/graphql/rootQuery.ts
@@ -1,43 +1,73 @@
 import {
-  GraphQLError,
   GraphQLList,
   GraphQLNonNull,
   GraphQLObjectType,
-  GraphQLString,
 } from 'graphql';
 import { UserType } from './types/user.js';
 import { PrismaClient } from '@prisma/client';
+import { MemberTypeId, MemberTypeType } from './types/memberType.js';
+import { PostType } from './types/post.js';
+import { ProfileType } from './types/profile.js';
+import { UUIDType } from './types/uuid.js';
 
 export const rootQuery = new GraphQLObjectType({
   name: 'Query',
   fields: {
     users: {
       type: new GraphQLList(UserType),
-      resolve: async (_root, _args, { prisma }: { prisma: PrismaClient }) => {
-        return await prisma.user.findMany();
-      },
+      resolve: async (_root, _args, { prisma }: { prisma: PrismaClient }) =>
+        await prisma.user.findMany(),
     },
     user: {
-      type: UserType,
-      args: {
-        id: { type: new GraphQLNonNull(GraphQLString) },
-      },
+      type: UserType as GraphQLObjectType,
+      args: { id: { type: new GraphQLNonNull(UUIDType) } },
       resolve: async (
         _root,
         { id } : { id: string },
         { prisma }: { prisma: PrismaClient }
-      ) => {
-        const user = await prisma.user.findUnique({ where: { id } });
-        if (user === null) {
-          throw new GraphQLError('User not found', {
-            extensions: {
-              code: 'NOT_FOUND',
-              http: { status: 404 },
-            },
-          });
-        }
-        return user;
-      },
-    }
+      ) => await prisma.user.findUnique({ where: { id } }),
+    },
+    memberTypes: {
+      type: new GraphQLList(MemberTypeType),
+      resolve: async (_root, _args, { prisma }: { prisma: PrismaClient }) =>
+        await prisma.memberType.findMany(),
+    },
+    memberType: {
+      type: MemberTypeType,
+      args: { id: { type: new GraphQLNonNull(MemberTypeId) } },
+      resolve: async (
+        _root,
+        { id } : { id: string },
+        { prisma }: { prisma: PrismaClient }
+      ) => await prisma.memberType.findUnique({ where: { id } }),
+    },
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve: async (_root, _args, { prisma }: { prisma: PrismaClient }) =>
+        await prisma.post.findMany(),
+    },
+    post: {
+      type: PostType,
+      args: { id: { type: new GraphQLNonNull(UUIDType) } },
+      resolve: async (
+        _root,
+        { id } : { id: string },
+        { prisma }: { prisma: PrismaClient }
+      ) => await prisma.post.findUnique({ where: { id } }),
+    },
+    profiles: {
+      type: new GraphQLList(ProfileType),
+      resolve: async (_root, _args, { prisma }: { prisma: PrismaClient }) =>
+        await prisma.profile.findMany(),
+    },
+    profile: {
+      type: ProfileType,
+      args: { id: { type: new GraphQLNonNull(UUIDType) } },
+      resolve: async (
+        _root,
+        { id } : { id: string },
+        { prisma }: { prisma: PrismaClient }
+      ) => await prisma.profile.findUnique({ where: { id } }),
+    },
   },
 });
